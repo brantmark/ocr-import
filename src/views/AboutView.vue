0e7f4fd1a5d3a5051d5bd5c3beec44219bd0efec
Benjamin B <bennibryne+brantmark@gmail.com>
2022-10-31 13:18:17 +0100

    chore: add tesseract poc

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { createWorker } from 'tesseract.js'
+import { onMounted } from 'vue'
+
+const worker = createWorker({
+	logger: (m) => console.log(m),
+})
+
+// functions
+const parse = async () => {
+	await worker.load()
+	await worker.loadLanguage('eng')
+	await worker.initialize('eng')
+	const {
+		data: { text },
+	} = await worker.recognize(
+		'https://tesseract.projectnaptha.com/img/eng_bw.png'
+	)
+	console.log(text)
+	await worker.terminate()
+}
+onMounted(() => parse())
+</script>
+
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
